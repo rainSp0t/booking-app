607dfaa095a500dddded7f3eb0c962a62f447e7b
rainSp0t <baculomiko17@gmail.com>
2026-07-22 15:57:55 +1200

    Add Entity Framework Core and ApplicationDbContext

---
+++ backend/BookingApp/BookingApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingApp.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Database tables will go here.
+}

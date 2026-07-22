86080cc4f4711bae8a6155c2c16f75845c87e0e3
rainSp0t <baculomiko17@gmail.com>
2026-07-22 19:38:53 +1200

    Add venue creation DTO

---
+++ backend/BookingApp/BookingApp/DTOs/Venue/CreateVenueDto.cs
@@ -0,0 +1,14 @@
+﻿namespace BookingApp.DTOs.Venue;
+
+public class CreateVenueDto
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Address { get; set; } = string.Empty;
+
+    public string ContactNumber { get; set; } = string.Empty;
+
+    public int OwnerId { get; set; }
+}

9db4d838280de2e00b4027259ca1e9b4f0fdb627
rainSp0t <baculomiko17@gmail.com>
2026-07-23 22:50:48 +1200

    Add opening hours creation endpoint

---
+++ backend/BookingApp/BookingApp/Controllers/OpeningHoursController.cs
@@ -0,0 +1,40 @@
+﻿using BookingApp.Data;
+using BookingApp.DTOs.OpeningHours;
+using BookingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OpeningHoursController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public OpeningHoursController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOpeningHours(CreateOpeningHoursDto dto)
+    {
+        var openingHours = new OpeningHours
+        {
+            VenueId = dto.VenueId,
+            DayOfWeek = dto.DayOfWeek,
+            OpenTime = dto.OpenTime,
+            CloseTime = dto.CloseTime,
+            IsClosed = dto.IsClosed
+        };
+
+
+        _context.OpeningHours.Add(openingHours);
+
+        await _context.SaveChangesAsync();
+
+
+        return Ok(openingHours);
+    }
+}
---
+++ backend/BookingApp/BookingApp/DTOs/OpeningHours/CreateOpeningHoursDto.cs
@@ -0,0 +1,14 @@
+﻿namespace BookingApp.DTOs.OpeningHours;
+
+public class CreateOpeningHoursDto
+{
+    public int VenueId { get; set; }
+
+    public DayOfWeek DayOfWeek { get; set; }
+
+    public TimeSpan OpenTime { get; set; }
+
+    public TimeSpan CloseTime { get; set; }
+
+    public bool IsClosed { get; set; }
+}

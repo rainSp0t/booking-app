fb64d0d7ff06814043d7834409e0175be5fc9475
rainSp0t <baculomiko17@gmail.com>
2026-07-23 19:37:49 +1200

    Add user registration endpoint

---
+++ backend/BookingApp/BookingApp/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using BookingApp.Data;
+using BookingApp.DTOs.User;
+using BookingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public UsersController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterUserDto dto)
+    {
+        var user = new User
+        {
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            Email = dto.Email,
+
+            // Temporary
+            PasswordHash = dto.Password,
+
+            Role = dto.Role,
+            CreatedDate = DateTime.UtcNow
+        };
+
+        _context.Users.Add(user);
+
+        await _context.SaveChangesAsync();
+
+        return Ok(user);
+    }
+}
